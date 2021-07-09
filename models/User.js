37504bc8ec764877d1520ee32076c9055b09a08e
jasonmueller <jmuel9999@gmail.com>
2021-07-09 17:24:05 -0500

    modified User.js

--- models/User.js
+++ models/User.js
@@ -3,11 +3,31 @@ const { Schema, model } = require('mongoose');
 const UserSchema = new Schema(
     {
         userName: {
-           type: String
+            type: String,
+            unique: true,
+            require: true,
+            trim: true,
         },
         email: {
-            type: String
+            type: String,
+            require: true,
+            unique: true,
         },
+        thoughts: {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        },
+        friends: {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+          },
+          id: false 
     }
 );
 
